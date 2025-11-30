1b78be53abf43ca2c80e4ff3c91923f592515961
LilianaRache <lilianarache@gmail.com>
2025-11-30 15:29:34 -0500

    Add docker domain to cors rule

--- src/main/java/com/referencedpaymentsapi/config/WebSecurityConfig.java
+++ src/main/java/com/referencedpaymentsapi/config/WebSecurityConfig.java
@@ -69,7 +69,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://referenced-payments-frontend:4200"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
